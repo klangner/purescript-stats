eaa903cd775a6bd88f3395859e48a3579e54ed39
klangner <klangner@gmail.com>
2017-04-14 19:55:55 +0200

    Implemented Uniform distribution

---
+++ src/Statistics/Distribution/Uniform.purs
@@ -0,0 +1,58 @@
+-- Variate distributed uniformly in the interval.
+
+module Statistics.Distribution.Uniform
+    ( UniformDistribution(..)
+    -- * Constructors
+    , uniformDistr
+    , uniformDistrE
+    ) where
+
+import Prelude
+import Data.Maybe(Maybe(..))
+import Math (log)
+import Statistics.Distribution (class ContDistr, class Distribution, cumulative, density)
+
+
+-- | Uniform distribution from A to B
+data UniformDistribution = UD { uniformA :: Number -- ^ Low boundary of distribution
+                              , uniformB :: Number -- ^ Upper boundary of distribution
+                              }
+
+-- Show instance
+instance showUniform :: Show UniformDistribution where
+  show (UD {uniformA, uniformB}) = "Uniform distribution: {A: " <> show uniformA <> ", B: " <> show uniformB <> "}"
+
+-- Equality instance
+instance eqNormal :: Eq UniformDistribution where
+  eq (UD {uniformA: a1, uniformB: b1}) (UD {uniformA: a2, uniformB: b2}) = a1 == a2 && b1 == b2
+
+-- Distribution instance
+instance distrUniform :: Distribution UniformDistribution where
+    cumulative d x = 0.0
+
+    complCumulative d x = 1.0 - cumulative d x
+
+-- Contiuous Distribution instance
+instance contDistrUniform :: ContDistr UniformDistribution where
+    density (UD {uniformA, uniformB}) x
+      | x < uniformA     = 0.0
+      | x > uniformB     = 0.0
+      | otherwise = 1.0 / (uniformB - uniformA)
+
+    logDensity d = log <<< density d
+  
+
+-- | Create normal distribution from mean and standard deviation
+-- | Use this function only when you are sure that sd > 0.
+-- | Otherwise use safe normalDistrE
+uniformDistr :: Number -> Number -> UniformDistribution
+uniformDistr a b = UD {uniformA: a, uniformB: b}
+
+
+-- | Create normal distribution in a safe way
+uniformDistrE :: Number -> Number -> Maybe UniformDistribution
+uniformDistrE a b
+  | b < a     = Just $ UD {uniformA: b, uniformB: a}
+  | a < b     = Just $ UD {uniformA: a, uniformB: b}
+  | otherwise = Nothing
+
--- test/Test/Statistics/Distribution.purs
+++ test/Test/Statistics/Distribution.purs
@@ -10,6 +10,7 @@ import Test.QuickCheck (class Arbitrary
 import Test.QuickCheck.Gen (choose)
 import Statistics.Distribution (class ContDistr, density)
 import Statistics.Distribution.Normal (NormalDistribution, normalDistr)
+import Statistics.Distribution.Uniform (UniformDistribution, uniformDistr)
 
 
 -- Helper type to define Arbitrary instances. Without this type they will be orphaned
@@ -22,23 +23,44 @@ instance arbNormal :: Arbitrary (D NormalDistribution) where
         std <- choose 1e-3 1e6
         pure $ D $ normalDistr mu std
 
+instance arbUniform :: Arbitrary (D UniformDistribution) where
+    arbitrary = do
+        a <- arbitrary
+        b <- arbitrary
+        let b' = if a == b then a + 1.0 else b
+        pure $ D $ uniformDistr a b'
+
 
 -- Main test method
 testDistribution :: ∀ eff. QC eff Unit
 testDistribution = do
+    
     log "\n# Distribution tests"
 
     log "## Normal distribution"
     normalDistrTests 
 
+    log "## Uniform distribution"
+    uniformDistrTests 
+
 
 -- Test normal distributions
 normalDistrTests :: ∀ eff. QC eff Unit
 normalDistrTests= do
+    
     log "* PDF sanity"
     quickCheck $ \(D d :: D NormalDistribution) x -> 
         pdfSanityCheck d x <?> "Distribution: " <> show d <> ", x: " <> show x
 
+
+-- Test uniform distributions
+uniformDistrTests :: ∀ eff. QC eff Unit
+uniformDistrTests= do
+    
+    log "* PDF sanity"
+    quickCheck $ \(D d :: D UniformDistribution) x -> 
+        pdfSanityCheck d x <?> "Distribution: " <> show d <> ", x: " <> show x
+    
 
 -- PDF is always positive
 pdfSanityCheck :: ∀ t. ContDistr t => t -> Number -> Boolean
