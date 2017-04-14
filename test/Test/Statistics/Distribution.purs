fdd43a442a9daded27824af1e2678952fc3def2f
klangner <klangner@gmail.com>
2017-04-14 19:14:19 +0200

    Working QC tests

--- test/Test/Statistics/Distribution.purs
+++ test/Test/Statistics/Distribution.purs
@@ -1,63 +1,45 @@
 module Test.Statistics.Distribution (testDistribution) where
 
 import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (log, CONSOLE)
-import Control.Monad.Eff.Exception (EXCEPTION)
-import Control.Monad.Eff.Random (RANDOM)
+import Control.Monad.Eff.Console (log)
 import Test.QuickCheck (class Arbitrary
+                       , QC
                        , arbitrary
                        , quickCheck
                        , (<?>))
 import Test.QuickCheck.Gen (choose)
-import Statistics.Distribution (density)
+import Statistics.Distribution (class ContDistr, density)
 import Statistics.Distribution.Normal (NormalDistribution, normalDistr)
 
 
+-- Helper type to define Arbitrary instances. Without this type they will be orphaned
+-- And this file won't compile
 newtype D a = D a 
 
 instance arbNormal :: Arbitrary (D NormalDistribution) where
     arbitrary = do
         mu <- arbitrary
-        std <- choose 0.1 1000000.0
+        std <- choose 1e-3 1e6
         pure $ D $ normalDistr mu std
 
 
-testDistribution :: ∀ eff. Eff (console :: CONSOLE, random :: RANDOM, exception :: EXCEPTION | eff) Unit
+-- Main test method
+testDistribution :: ∀ eff. QC eff Unit
 testDistribution = do
+    log "\n# Distribution tests"
 
-    log "Normal distribution"
-    contDistrTests --(T :: T NormalDistribution )
+    log "## Normal distribution"
+    normalDistrTests 
 
 
--- Tests for continuous distributions
-contDistrTests :: ∀ eff. Eff (console :: CONSOLE, random :: RANDOM, exception :: EXCEPTION | eff) Unit
-contDistrTests = do
-    log "PDF sanity"
-    quickCheck $ \(D d) x -> 
+-- Test normal distributions
+normalDistrTests :: ∀ eff. QC eff Unit
+normalDistrTests= do
+    log "* PDF sanity"
+    quickCheck $ \(D d :: D NormalDistribution) x -> 
         pdfSanityCheck d x <?> "Distribution: " <> show d <> ", x: " <> show x
 
 
--- PDF is positive
-pdfSanityCheck :: NormalDistribution -> Number -> Boolean
+-- PDF is always positive
+pdfSanityCheck :: ∀ t. ContDistr t => t -> Number -> Boolean
 pdfSanityCheck d x = (density d x) >= 0.0
-
-
-
---   , testProperty "Quantile is CDF inverse" $ quantileIsInvCDF   t
---   , testProperty "quantile fails p<0||p>1" $ quantileShouldFail t
---   , testProperty "log density check"       $ logDensityCheck    t
---   , testProperty "complQuantile"           $ complQuantileCheck t
-
-
--- Tests for distributions which have CDF
--- cdfTests :: (Param d, Distribution d, QC.Arbitrary d, Show d) => T d -> [Test]
--- cdfTests t =
---   [ testProperty "C.D.F. sanity"        $ cdfSanityCheck         t
---   , testProperty "CDF limit at +inf"    $ cdfLimitAtPosInfinity  t
---   , testProperty "CDF limit at -inf"    $ cdfLimitAtNegInfinity  t
---   , testProperty "CDF at +inf = 1"      $ cdfAtPosInfinity       t
---   , testProperty "CDF at -inf = 1"      $ cdfAtNegInfinity       t
---   , testProperty "CDF is nondecreasing" $ cdfIsNondecreasing     t
---   , testProperty "1-CDF is correct"     $ cdfComplementIsCorrect t
--- ]
--- test/Test/Statistics/Sample.purs
+++ test/Test/Statistics/Sample.purs
@@ -11,14 +11,15 @@ import Statistics.Sample as S
 testSample :: forall eff. Eff (console :: CONSOLE, assert :: ASSERT | eff) Unit
 testSample = do
 
+    log "\n# Test Sample data structure"
     let s1 = [1.0, 2.0, 3.0]
     let s2 = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0]
 
-    log "mean"
+    log "* mean"
     assert $ S.mean s1 == 2.0
 
-    log "variance"
+    log "* variance"
     assert $ S.variance s2 == 35.0/12.0
 
-    log "standard deviation"
+    log "* standard deviation"
     assert $ S.stddev s2 == sqrt (35.0/12.0)
